091549b4ee1d5f46cbd2c1a9dcc8ed302347c6b8
Jan Philipp Weber <10946850+jp-weber@users.noreply.github.com>
2019-11-21 17:37:59 +0100

    WIP new FileService

--- Source/Template10.Extras/Services/File/Abstractions/IFileService.cs
+++ Source/Template10.Extras/Services/File/Abstractions/IFileService.cs
@@ -5,12 +5,67 @@ namespace Template10.Services.File
 {
     public interface IFileService
     {
-        Task<bool> DeleteFileAsync(string key, StorageStrategies location = StorageStrategies.Local);
-        Task<bool> FileExistsAsync(string key, StorageStrategies location = StorageStrategies.Local);
+        /// <summary>Deletes a file in the specified storage strategy</summary>
+        /// <param name="key">Path of the file in storage</param>
+        /// <param name="location">Location storage strategy</param>
+        /// <param name="path">Custom path for storage. Only accessible if rights have been granted</param>
+        /// <returns>Boolean: true if deleted, false if not deleted</returns>
+        Task<bool> DeleteFileAsync(string key, StorageStrategies location = StorageStrategies.Local, string path = null);
+
+        /// <summary>Deletes a file in the specified storage strategy</summary>
+        /// <param name="key">Path of the file in storage</param>
+        /// <param name="folder">Parent folder of the file. Only accessible if rights have been granted</param>
+        /// <returns>Boolean: true if deleted, false if not deleted</returns>
+        Task<bool> DeleteFileAsync(string key, StorageFolder folder);
+
+        /// <summary>Returns if a file is found in the specified storage strategy</summary>
+        /// <param name="key">Path of the file in storage</param>
+        /// <param name="location">Location storage strategy</param>
+        /// <param name="path">Custom path for storage. Only accessible if rights have been granted</param>
+        /// <returns>Boolean: true if found, false if not found</returns>
+        Task<bool> FileExistsAsync(string key, StorageStrategies location = StorageStrategies.Local, string path = null);
+
+        /// <summary>Returns if a file is found in the specified storage strategy</summary>
+        /// <param name="key">Path of the file in storage</param>
+        /// <param name="folder">Parent folder of the file. Only accessible if rights have been granted</param>
+        /// <returns>Boolean: true if found, false if not found</returns>
         Task<bool> FileExistsAsync(string key, StorageFolder folder);
-        Task<T> ReadFileAsync<T>(string key, StorageStrategies location = StorageStrategies.Local);
-        Task<string> ReadStringAsync(string key, StorageStrategies location = StorageStrategies.Local);
-        Task<bool> WriteFileAsync<T>(string key, T value, StorageStrategies location = StorageStrategies.Local, CreationCollisionOption option = CreationCollisionOption.ReplaceExisting);
-        Task<bool> WriteStringAsync(string key, string value, StorageStrategies location = StorageStrategies.Local, CreationCollisionOption option = CreationCollisionOption.ReplaceExisting);
+
+        /// <summary>Reads and deserializes a file into specified type T</summary>
+        /// <typeparam name="T">Specified type into which to deserialize file content</typeparam>
+        /// <param name="key">Path to the file in storage</param>
+        /// <param name="location">Location storage strategy</param>
+        /// <param name="path">Custom path for storage. Only accessible if rights have been granted</param>
+        /// <returns>Specified type T</returns>
+        Task<T> ReadFileAsync<T>(string key, StorageStrategies location = StorageStrategies.Local, string path = null);
+
+        /// <summary>Reads and deserializes a file into specified type T</summary>
+        /// <typeparam name="T">Specified type into which to deserialize file content</typeparam>
+        /// <param name="key">Path to the file in storage</param>
+        /// <param name="folder">Parent folder of the file. Only accessible if rights have been granted</param>
+        /// <returns>Specified type T</returns>
+        Task<T> ReadFileAsync<T>(string key, StorageFolder folder);
+
+        /// <summary>Reads and deserializes a file as string</summary>
+        /// <param name="key">Path to the file in storage</param>
+        /// <param name="location">Location storage strategy</param>
+        /// <param name="path">Custom path for storage. Only accessible if rights have been granted</param>
+        /// <returns>string</returns>
+        Task<string> ReadStringAsync(string key, StorageStrategies location = StorageStrategies.Local, string path = null);
+
+        /// <summary>Reads and deserializes a file as string</summary>
+        /// <param name="key">Path to the file in storage</param>
+        /// <param name="folder">Parent folder of the file. Only accessible if rights have been granted</param>
+        /// <returns>string</returns>
+        Task<string> ReadStringAsync(string key, StorageFolder folder);
+
+
+        Task<bool> WriteFileAsync<T>(string key, T value, StorageStrategies location = StorageStrategies.Local, CreationCollisionOption option = CreationCollisionOption.ReplaceExisting, string path = null);
+
+        Task<bool> WriteFileAsync<T>(string key, T value, StorageFolder folder);
+
+        Task<bool> WriteStringAsync(string key, string value, StorageStrategies location = StorageStrategies.Local, CreationCollisionOption option = CreationCollisionOption.ReplaceExisting, string path = null);
+
+        Task<bool> WriteStringAsync<T>(string key, string value, StorageFolder folder);
     }
 }
